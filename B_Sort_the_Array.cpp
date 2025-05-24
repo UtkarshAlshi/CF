6e83d470f2b822dd715f303ae518cce0bd22f407
Utkarsh Alshi <117593889+UtkarshAlshi@users.noreply.github.com>
2025-05-24 06:51:53 +0530

    Add files via upload

---
+++ B_Sort_the_Array.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define F first
+#define S second
+#define mp make_pair
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define rloop(i, a, b) for (int i = a; i >= b; i--)
+#define print(x) cout << x << endl;
+#define setbits(x) __builtin_popcount(x)
+#define zrobits(x) __builtin_ctz(x)
+#define sp(x, y) fixed << setprecision(y) << x
+#define mod 1000000007
+#define inf 1e18
+#define PI 3.14159265358979323846
+#define M 998244353
+#define MAX 1000000
+#define EPS 1e-9
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define sz(x) (int)x.size()
+#define sp(x, y) fixed << setprecision(y) << x
+#define mem(a, b) memset(a, b, sizeof(a))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / __gcd(a, b)
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+
+void solve() {
+    int n;
+    cin >> n;
+    vi v(n);
+    loop(i, 0, n) cin >> v[i];
+    vi w = v;
+    sort(all(w));
+    int l = 0, r = n - 1;
+    while (l < n && v[l] == w[l]) l++;
+    while (r >= 0 && v[r] == w[r]) r--;
+    if (l >= r) {
+        print("yes");
+        print("1 1");
+        return;
+    }
+    reverse(v.begin() + l, v.begin() + r + 1);
+    if (v == w) {
+        print("yes");
+        cout << l + 1 << " " << r + 1 << endl;
+    } else {
+        print("no");
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+    int t = 1;
+  //  cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
