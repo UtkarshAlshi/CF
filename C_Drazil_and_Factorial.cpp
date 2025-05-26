361e6a8bc6086d8f4eac53243f9b10f89be8c057
Utkarsh Alshi <117593889+UtkarshAlshi@users.noreply.github.com>
2025-05-26 12:44:06 +0530

    Add files via upload

---
+++ C_Drazil_and_Factorial.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define F first
+#define S second
+#define mp make_pair
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define rloop(i, a, b) for (int i = a; i >= b; i--)
+#define print(x) cout << x << endl;
+#define setbits(x) __builtin_popcount(x)
+#define zrobits(x) __builtin_ctz(x)
+#define sp(x, y) fixed << setprecision(y) << x
+#define mod 1000000007
+#define inf 1e18
+#define PI 3.14159265358979323846
+#define M 998244353
+#define MAX 1000000
+#define EPS 1e-9
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define sz(x) (int)x.size()
+#define sp(x, y) fixed << setprecision(y) << x
+#define mem(a, b) memset(a, b, sizeof(a))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / __gcd(a, b)
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+    vi v;
+    for (auto x : s) {
+        if (x == '0' || x == '1') continue;
+        if (x == '4') v.pb(3), v.pb(2), v.pb(2);
+        else if (x == '6') v.pb(5), v.pb(3);
+        else if (x == '8') v.pb(7), v.pb(2), v.pb(2), v.pb(2);
+        else if (x == '9') v.pb(7), v.pb(3), v.pb(3), v.pb(2);
+        else v.pb(x - '0');
+    }
+    sort(all(v), greater<int>());
+    for (auto x : v) cout << x;
+    cout << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+    int t = 1;
+   // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
